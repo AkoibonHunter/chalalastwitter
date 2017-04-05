91fa2643205563601ab6bbd1c78dd2ff89c2b743
root <root@localhost.localdomain>
2017-04-05 20:27:50 +0000

    commit 3

--- basefunction.py
+++ basefunction.py
@@ -1,5 +1,5 @@
-def countlettre (lettre, txt):
-	#coder ici
-	return "le nombre de lettre dans txt"
+	def countlettre (lettre, txt):
+		print txt[10]
+		return "le nombre de lettre dans txt"
 
 print countlettre('e', "ilyaauemoinseee et ee ca fait5")
