b1d07fd0fa4b9aa154c566f5bbce2e1d54c80604
root <root@localhost.localdomain>
2017-04-05 21:54:58 +0000

    commit 4

--- basefunction.py
+++ basefunction.py
@@ -1,5 +1,38 @@
-	def countlettre (lettre, txt):
-		print txt[10]
-		return "le nombre de lettre dans txt"
+import urllib2
 
-print countlettre('e', "ilyaauemoinseee et ee ca fait5")
+def countlettre (lettreacompter, txt):
+	i = 0     #compteur
+	
+	for l in txt :    #lettre
+		if l == lettreacompter:
+			i = i+1
+	return i
+
+
+#novuellefcno
+
+
+
+#main
+f    = urllib2.urlopen("http://pi.com")
+data = f.read()
+f.close()
+print ilya
+print
+print countlettre('a', data)
+print countlettre('t', data)
+
+#data = "tetzetjzekltj" 
+#compter le nombre pour chaque lettre de l'alphabet -
+#
+
+#fonction#
+#in 2 variables : txt lettre
+#out txt enlever lettre
+
+# txt =salut , lettre = t
+# out salu
+
+
+
+
